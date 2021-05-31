f0e926917e9c198b5f16b31b35ce1a1d3904d224
Kiefer Levine <kiefer_8@hotmail.com>
2021-05-30 20:50:25 -0400

    Page now calls the wger API and stores one workout from it
    This is an important step in creating the user-chosen experience. It's randomized now, but the next step will be to base it on what the user submits.

--- assets/js/muscle.js
+++ assets/js/muscle.js
@@ -13,9 +13,12 @@ const apiUrl = "https://wger.de/api/v2/exerciseinfo?";
 const english = "language=2"
 
 // Get the data from wger
-const getWorkoutData = function() {
+const getExerciseData = function() {
+    // This is the length of the wger API's english-language list of exercises, minus 20. Hard-coding it because it's inefficient to call it to see the length and then call it again; we can change this later if it proves unwieldy. By subtracting 20, I get a value between 0 and 209, so I always get the max of 20 items in the response. In any case, it's used to create a random position within the total 
+    const countLength = 209;
+    const randomOffest = "&offset=" + Math.floor(Math.random() * countLength);
     // Here's the fetch, with the url cobbled together from input and base values
-    fetch(apiUrl + english)
+    fetch(apiUrl + english + randomOffest)
         .then(function(response) {
             // If the response is good,
             if (response.ok) {
@@ -23,14 +26,27 @@ const getWorkoutData = function() {
                 response.json()
                     .then(function(data) {
                         console.log(data);
-                    })
+                        // Now we choose which exercise
+                        chooseExercise(data);
+                    });
             }
         });
 };
 
-const makeInputData = function() {
+const chooseExercise = function(data) {
+    // Now, we pick one of the workouts from the list, provided it meets the requirements that the user provided.
+    const randomExercise = Math.floor(Math.random() * data.results.length);
+    const workoutData = data.results[randomExercise];
+    
+    localStorage.setItem("workoutData", JSON.stringify(workoutData))
+};
 
-}
+const makeInputData = function() {
+    // First, clear the existing localStorage data for the previous exercise
+    localStorage.removeItem("workoutData");
+    // Then we get the data for the exercise
+    getExerciseData();
+};
 
 // Increases the value of the muscle group when it is clicked, or cycles it back to 0 if it reaches the maximum
 const incrementVal = function(event) {
@@ -54,6 +70,4 @@ shouldersValEl.addEventListener("click", incrementVal);
 bicepsValEl.addEventListener("click", incrementVal);
 tricepsValEl.addEventListener("click", incrementVal);
 // The click event for leaving the page; which is where all the magic happens
-nextPageEl.addEventListener("click", function() {
-    console.log("Hey");
-});
+nextPageEl.addEventListener("click", makeInputData);
