267c41a0edc3c6ad757cd5c3332b26dcb0875b70
Riz <80712058+rizznn@users.noreply.github.com>
2021-06-01 22:25:29 -0400

    save to and retrieve from localStorage

--- assets/js/name.js
+++ assets/js/name.js
@@ -1,35 +1,18 @@
 var nextEl = document.querySelector(".nextBtn");
 var inputEl = document.querySelector(".validate");
 
-
-
-// function getName() {
-//     return localStorage.getItem("userName");
-// }
-
-// function update() {
-//     var name = getName();
-//     // document.querySelector("greet").innerHTML = "Welcome " + name + "!";
-//     document.querySelector("storedName").innerHTML = name;
-// }
-
 nextEl.addEventListener("click", function(event){
     event.preventDefault();
     // Gets input value
     var name = inputEl.value;
-    console.log(name);
-    // Saves data to retrieve later
-    localStorage.setItem("userName", name);
-    alert('clicked!');
+    var userData = { userName: `${name}` };
+
+    // Saves data to storage
+    localStorage.setItem("userData", JSON.stringify(userData));
 });
 
-// function getInput() {
-//     // Gets input value
-//     var name = input.value;
+// Retrieve the object from storage
+var getObject = localStorage.getItem('userData');
 
-//     // Saves data to retrieve later
-//     localStorage.setItem("userName", name);
+console.log('getObject: ', JSON.parse(getObject));
 
-//     // update();
-//     console.log(name);
-// }
