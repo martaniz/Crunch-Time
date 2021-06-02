a190ac73c6c762ee3d61207640f81d914fe53591
NayanSidhu <nsidh068@uottawa.ca>
2021-06-02 03:35:01 -0400

    added user time selector

---
+++ assets/js/counter.js
@@ -0,0 +1,9 @@
+var slider = document.getElementById("myRange");
+var output = document.getElementById("value");
+
+
+output.innerHTML = slider.value;
+
+slider.oninput = function(){
+    output.innerHTML = this.value;
+}
