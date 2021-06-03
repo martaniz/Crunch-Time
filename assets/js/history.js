cb5a37597f95ee9b5ade81d5edf9e8e308c24f93
Riz <80712058+rizznn@users.noreply.github.com>
2021-06-03 08:07:11 -0400

    display workout name in table

--- assets/js/history.js
+++ assets/js/history.js
@@ -4,12 +4,12 @@ function retrieveWorkout() {
         // Retrieve the object from storage and converts string to array
         var getWorkout = JSON.parse(localStorage.getItem('userData'));
     
-        console.log(getWorkout);
-
         var getWorkoutName = getWorkout.chosenWorkout.name;
         console.log(getWorkoutName);
 
-       
+        var para = document.createElement("p");
+        para.textContent = getWorkoutName;
+        tableData.appendChild(para);
 
 }
 retrieveWorkout();
