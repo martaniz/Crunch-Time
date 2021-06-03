b2447ccfc1423e3e080be4b12ac85f721b70b7ac
Kiefer Levine <kiefer_8@hotmail.com>
2021-06-02 21:33:21 -0400

    Made data flow correctly between pages; fixed favicons

--- assets/js/counter.js
+++ assets/js/counter.js
@@ -3,6 +3,14 @@ var output = document.getElementById("value");
 var rightArrow = document.getElementById("rightArrow");
 var userData = {};
 
+var loadUserData = function() {
+    var data = JSON.parse(localStorage.getItem("userData"));
+
+    if (data) {
+        userData = data;
+    }
+};
+
 output.innerHTML = slider.value;
 
 slider.oninput = function(){
@@ -10,7 +18,9 @@ slider.oninput = function(){
     userData.userTime = output.textContent*60; 
 }
 
-
 rightArrow.addEventListener("click", function(){
     localStorage.setItem("userData", JSON.stringify(userData));
 })
+
+
+loadUserData();
