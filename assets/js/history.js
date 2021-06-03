e3a7859e3b159c86fc73e3917a1021ba7571f3d7
Riz <80712058+rizznn@users.noreply.github.com>
2021-06-02 21:42:53 -0400

    get data from local storage

--- assets/js/history.js
+++ assets/js/history.js
@@ -1,8 +1,9 @@
 function retrieveWorkout() {
         // Retrieve the object from storage
-        var getWorkout = localStorage.getItem('workoutData');
+        var getWorkout = localStorage.getItem('userData');
         
         console.log(JSON.parse(getWorkout));
         // converts string back to array
         // return JSON.parse(getWorkout);
 }
+retrieveWorkout();
