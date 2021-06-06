2caec46df700337efde0108d67e486d679b48f62
Riz <80712058+rizznn@users.noreply.github.com>
2021-06-05 20:23:34 -0400

    displaying workouts

--- assets/js/history.js
+++ assets/js/history.js
@@ -1,27 +1,56 @@
 var tableData = document.querySelector(".td");
 var tableTime = document.querySelector(".ttime");
 
+// function reason(){
+//     var dropd = document.getElementById("savedrop").value;
+//     var drophistory = JSON.parse(localStorage.getItem("reason")) || [];
+//     drophistory.push(dropd);
+//     localStorage.setItem("reason", JSON.stringify(drophistory));
+// }
+var getWorkout = [];
 function retrieveWorkout() {
-        // Retrieve the object from storage and converts string to array
-        var getWorkout = JSON.parse(localStorage.getItem('userData'));
+    // Retrieve the object from storage and converts string to array
+    var userData = JSON.parse(localStorage.getItem('userData'));
+
+    // history array from local storage, if its empty, I'll make a new one
+    var historyArr = JSON.parse(localStorage.getItem("historyArr"));
+    if (historyArr === null ){
+        historyArr = [];
+    }
+
+    // getting the name of the workout and push it to historyArr
+    historyArr.push(userData);
     
-        var getWorkoutName = getWorkout.chosenWorkout.name;
+    // for loop to print to page
+    for (i = 0; i <= historyArr.length; i++ ) {
+        var para = document.createElement("p");
+        para.textContent = historyArr[i].chosenWorkout.name;
+        tableData.appendChild(para);
 
         var para = document.createElement("p");
-        para.textContent = getWorkoutName;
+        para.textContent = historyArr[i].c
         tableData.appendChild(para);
-}
-retrieveWorkout();
 
-function retrieveTime() {
-    // Retrieve the object from storage
-    var getObject = JSON.parse(localStorage.getItem('userData'));
+        var para = document.createElement("p");
+        para.textContent = historyArr[i].chosenWorkout.name;
+        tableData.appendChild(para);
 
-    var getTime = getObject.userTime;
+    }
+    // save to historyArr to historyArr
+    localStorage.setItem("historyArr", JSON.stringify(historyArr));
+
+    
+
+
+    // var getTime = getObject.userTime;
+    
+    // getObject.push(getTime);
+
+    // localStorage.setItem("userData", JSON.stringify(getObject));
 
     var para = document.createElement("p");
     para.textContent = getTime;
     tableTime.appendChild(para);
 
 }
-retrieveTime();
+retrieveWorkout();
