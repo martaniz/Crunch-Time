81f5e0d89e09558105cf991e25337b19a1d208ac
Riz <80712058+rizznn@users.noreply.github.com>
2021-06-05 20:24:41 -0400

    working workout display

--- assets/js/history.js
+++ assets/js/history.js
@@ -1,43 +1,91 @@
 var tableData = document.querySelector(".td");
 var tableTime = document.querySelector(".ttime");
 
-// function reason(){
-//     var dropd = document.getElementById("savedrop").value;
-//     var drophistory = JSON.parse(localStorage.getItem("reason")) || [];
-//     drophistory.push(dropd);
-//     localStorage.setItem("reason", JSON.stringify(drophistory));
-// }
 var getWorkout = [];
-function retrieveWorkout() {
-    // Retrieve the object from storage and converts string to array
-    var userData = JSON.parse(localStorage.getItem('userData'));
+// Retrieve the object from storage and converts string to array
+var userData = JSON.parse(localStorage.getItem('userData'));
 
-    // history array from local storage, if its empty, I'll make a new one
-    var historyArr = JSON.parse(localStorage.getItem("historyArr"));
-    if (historyArr === null ){
-        historyArr = [];
+var workoutContainer = JSON.parse(localStorage.getItem("workoutContainer"));
+var timeContainer = JSON.parse(localStorage.getItem("timeContainer"));
+function retrieveWorkout() {
+    // workout array from local storage, if its empty, I'll make a new one
+    if (workoutContainer === null ){
+        workoutContainer = [];
     }
 
-    // getting the name of the workout and push it to historyArr
-    historyArr.push(userData);
+    // getting the data and push it to workoutContainer
+    // workoutContainer.push(userData);
+
+    workoutContainer.push(userData.chosenWorkout.name);
     
     // for loop to print to page
-    for (i = 0; i <= historyArr.length; i++ ) {
+    for (i = 0; i <= workoutContainer.length; i++ ) {
         var para = document.createElement("p");
-        para.textContent = historyArr[i].chosenWorkout.name;
+        para.textContent = workoutContainer[i];
         tableData.appendChild(para);
-
-        var para = document.createElement("p");
-        para.textContent = historyArr[i].c
-        tableData.appendChild(para);
-
-        var para = document.createElement("p");
-        para.textContent = historyArr[i].chosenWorkout.name;
-        tableData.appendChild(para);
-
     }
-    // save to historyArr to historyArr
-    localStorage.setItem("historyArr", JSON.stringify(historyArr));
+    console.log(workoutContainer);
+    // save to workoutContainer to workoutContainer
+    // localStorage.setItem("workoutContainer", JSON.stringify(workoutContainer));
+}
+retrieveWorkout();
+
+// function retrieveTime(){
+//     if (timeContainer === null ){
+//         timeContainer = [];
+//     }
+//     timeContainer.push(userData.userTime);
+    
+//     // for loop to print to page
+//     for (i = 0; i <= timeContainer.length; i++ ) {
+//         var para = document.createElement("p");
+//         para.textContent = timeContainer[i]/60 + " mins";
+//         tableTime.appendChild(para);
+//     }
+//     console.log(timeContainer);
+//     console.log(tableTime);
+//     // localStorage.setItem("timeContainer", JSON.stringify(timeContainer));
+// }
+// retrieveTime();
+
+// function retrieveName(){
+//     if (nameContainer === null ){
+//         nameContainer = [];
+//     }
+//     nameContainer.push(userData.userName);
+    
+//     // for loop to print to page
+//     for (i = 0; i <= nameContainer.length; i++ ) {
+//         var h2 = document.createElement("h2");
+//         para.textContent = nameContainer[i]/60 + " mins";
+//         userNameInput.appendChild(h2);
+//     }
+    
+//     localStorage.setItem("timeContainer", JSON.stringify(timeContainer));
+// }
+// retrieveTime();
+
+
+    // historyArr.push(userData.userName);
+    
+    // // for loop to print to page
+    // for (i = 0; i <= historyArr.length; i++ ) {
+    //     var para = document.createElement("p");
+    //     para.textContent = historyArr[i];
+    //     tableData.appendChild(para);
+    // }
+    
+    // historyArr.push(userData.userTime);
+    
+    // for loop to print to page
+    // for (i = 0; i <= historyArr.length; i++ ) {
+    //     var para = document.createElement("p");
+    //     para.textContent = historyArr[i];
+    //     tableTime.appendChild(para);
+    // }
+
+    
+
 
     
 
@@ -48,9 +96,8 @@ function retrieveWorkout() {
 
     // localStorage.setItem("userData", JSON.stringify(getObject));
 
-    var para = document.createElement("p");
-    para.textContent = getTime;
-    tableTime.appendChild(para);
+    // var para = document.createElement("p");
+    // para.textContent = getTime;
+    // tableTime.appendChild(para);
 
-}
-retrieveWorkout();
+
